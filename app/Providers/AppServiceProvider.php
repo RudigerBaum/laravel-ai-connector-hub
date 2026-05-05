0b849cef7a18c3a632be7dedda7a2b4794ab633f
Rüdiger Baumhart <rudigerbaumhart@gmail.com>
2026-05-05 14:22:49 +0200

    feat: Add AppServiceProvider

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        if ($this->app->environment('local')) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+        }
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
